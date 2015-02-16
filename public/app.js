02d11e30b1258c8119ad4dee796331478dcd0850
Kenny Howard <ken.chad.howard@gmail.com>
2015-02-15 19:06:07 -0700

    various updates

---
+++ public/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('benchedSports', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+	})
+})
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-/* 
-Need the following:
-npm init - package.json
-express
-body-parser
-*/
+var express = require('express');
+var Mongoose = require('mongoose')
+var bodyParser = require('body-parser');
+
+var app = express ();
+
+var port = 9001
+var mongoUri = 'mongodb://localhost:27017/benchedSports'
+
+app.use(bodyParse.json());
+
+mongoose.connect(mongoUri);
+mongoose.connection.once('open', function() {
+	console.log('Connected to Server: ' + mongoUri);
+});
+
+
+app.listen(port, function() {
+	console.log('Listening to port ' + port);
+});
