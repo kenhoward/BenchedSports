baa93470a754892512872d795ce8cd03b5168392
Kenny Howard <ken.chad.howard@gmail.com>
2015-02-17 16:43:13 -0700

    fixed dropdown issues

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,33 @@ var app = angular.module('benchedSports', ['ngRoute']);
 
 app.config(function($routeProvider) {
 	$routeProvider
-	.when('/', {
-		templateUrl: '/templates',
+	.when('/home', {
+		templateUrl: '/templates/home/home-view.html',
 		controller: 'homeCtrl'
 	})
+	.when('/nfl', {
+		templateUrl: '/templates/nfl-view.html',
+		controller: 'nflCtrl'
+	})	
+	.when('/mlb', {
+		templateUrl: '/templates/mlb-view.html',
+		controller: 'mlbCtrl'
+	})	
+	.when('/nba', {
+		templateUrl: '/templates/nba-view.html',
+		controller: 'nbaCtrl'
+	})	
+	.when('/soccer', {
+		templateUrl: '/templates/soccer-view.html',
+		controller: 'soccerCtrl'
+	})	
+	.when('/cricket', {
+		templateUrl: '/templates/cricket-view.html',
+		controller: 'cricketCtrl'
+	})	
+	.when('/running', {
+		templateUrl: '/templates/running-view.html',
+		controller: 'runningCtrl'
+	})
+	.otherwise('/home');
 })
