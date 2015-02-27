09bea326cec02f23188687b20ff8985f1655139e
Kenny Howard <ken.chad.howard@gmail.com>
2015-02-27 15:54:47 -0700

    update

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var app = express();
 // instance of the router
 // var adminRouter = express.Router()
 
-var port = 80 // previously 9001
-var mongoUri = 'mongodb://localhost:27017/benchedSports'
+var port = 'process.env.EXPRESS_PORT || 80' // previously 9001
+var mongoUri = 'mongodb://localhost:80:27017/benchedSports'
 
 // MIDDLEWARE ====================================================================================================
 
