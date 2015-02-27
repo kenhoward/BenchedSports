86b1899d855be88ee1162fb0747b05b227f63b9c
Kenny Howard <ken.chad.howard@gmail.com>
2015-02-27 16:00:52 -0700

    hosting issues

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var app = express();
 // instance of the router
 // var adminRouter = express.Router()
 
-var port = 'process.env.EXPRESS_PORT || 80' // previously 9001
-var mongoUri = 'mongodb://localhost:80:27017/benchedSports'
+var port = 'process.env.EXPRESS_PORT || 9001' // previously 9001
+var mongoUri = 'mongodb://localhost/benchedSports'
 
 // MIDDLEWARE ====================================================================================================
 
